b5f5e7b3d1cb6117723a7d1bf77ff8b48dffe3e5
Tyler Roscoe <tyler@cryptio.net>
2018-01-31 15:13:49 -0700

    Delete obsolete 1.3.3. Renumber.

--- 1.8/master/master_3_contoller_manager.sh
+++ 1.8/master/master_3_contoller_manager.sh
@@ -16,34 +16,27 @@ else
     warn "$check_1_3_2"
 fi
 
-check_1_3_3="1.3.3  - Ensure that the --insecure-experimental-approve-all-kubelet-csrs-for-group argument is not set"
-if check_argument 'kube-controller-manager' '--insecure-experimental-approve-all-kubelet-csrs-for-group' >/dev/null 2>&1; then
-    warn "$check_1_3_3"
-else
+check_1_3_3="1.3.3  - Ensure that the --use-service-account-credentials argument is set to true"
+if check_argument 'kube-controller-manager' '--use-service-account-credentials' >/dev/null 2>&1; then
     pass "$check_1_3_3"
+else
+    warn "$check_1_3_3"
 fi
 
-check_1_3_4="1.3.4  - Ensure that the --use-service-account-credentials argument is set to true"
-if check_argument 'kube-controller-manager' '--use-service-account-credentials' >/dev/null 2>&1; then
+check_1_3_4="1.3.4  - Ensure that the --service-account-private-key-file argument is set as appropriate"
+if check_argument 'kube-controller-manager' '--service-account-private-key-file' >/dev/null 2>&1; then
+    keyfile=$(get_argument_value 'kube-controller-manager' '--service-account-private-key-file')
     pass "$check_1_3_4"
+    pass "       * service-account-private-key-file: $keyfile"
 else
     warn "$check_1_3_4"
 fi
 
-check_1_3_5="1.3.5  - Ensure that the --service-account-private-key-file argument is set as appropriate"
-if check_argument 'kube-controller-manager' '--service-account-private-key-file' >/dev/null 2>&1; then
-    keyfile=$(get_argument_value 'kube-controller-manager' '--service-account-private-key-file')
-    pass "$check_1_3_5"
-    pass "       * service-account-private-key-file: $keyfile"
-else
-    warn "$check_1_3_5"
-fi
-
-check_1_3_6="1.3.6  - Ensure that the --root-ca-file argument is set as appropriate"
+check_1_3_5="1.3.5  - Ensure that the --root-ca-file argument is set as appropriate"
 if check_argument 'kube-controller-manager' '--root-ca-file' >/dev/null 2>&1; then
     cafile=$(get_argument_value 'kube-controller-manager' '--root-ca-file')
-    pass "$check_1_3_6"
+    pass "$check_1_3_5"
     pass "       * root-ca-file: $cafile"
 else
-    warn "$check_1_3_6"
+    warn "$check_1_3_5"
 fi
